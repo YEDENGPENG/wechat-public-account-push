e36371b4e00c2059a07048fa0920f7cd60e3eee3
YEDENGPENG <144139614+YEDENGPENG@users.noreply.github.com>
2023-10-09 22:39:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKI8W6ZDf-ji78SyIx2SfRKnQl6I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'qnPTer4Z4MuDWMYbHV-JXbe1T7b4Z6XYQst7nmM4e3E',
+      useTemplateId: 'BXzifeMc53RrEju5nnjwPjCsAIpCk9uxroIg8Bn8X88',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-18',
       festivals: [
@@ -54,7 +54,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKI8W6XT_uJKsfhrr1_bxYLZp6uw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'qnPTer4Z4MuDWMYbHV-JXbe1T7b4Z6XYQst7nmM4e3E',
+      useTemplateId: 'BXzifeMc53RrEju5nnjwPjCsAIpCk9uxroIg8Bn8X88',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-09',
       festivals: [
