08396ecf44847aee5945e571c87a5ca0d3b7168e
YEDENGPENG <144139614+YEDENGPENG@users.noreply.github.com>
2023-10-09 22:13:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '媇玥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oKI8W6Xek0xBOKZjVLl3EepSsBqM',
+      id: 'oKI8W6ZDf-ji78SyIx2SfRKnQl6I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'VYNOvKWKY_vW8SImuACjWImSC-i3mbe0JgumDaav4Vs',
+      useTemplateId: 'L4TX5bhqoRSjWnHPmhf1S7DiE56cdr2oZbT4WlBfjI4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-18',
       festivals: [
